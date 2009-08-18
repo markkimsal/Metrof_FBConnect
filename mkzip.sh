51ceca3d63fdd73249c52601504e6647347bb274
Mark Kimsal <mark2@trash2009.com>
2009-08-18 16:21:46 -0400

    Close UL

--- mkzip.sh
+++ mkzip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ../../
-zip -r Metrof_FBConnect_magento-1.0.5.zip Metrof/FBConnect/ -x "Metrof/FBConnect/.git*" -x "*~" -x "Metrof/FBConnect/mkzip.sh"
+zip -r Metrof_FBConnect_magento-1.0.6.zip Metrof/FBConnect/ -x "Metrof/FBConnect/.git*" -x "*~" -x "Metrof/FBConnect/mkzip.sh"
