453ca1520fb3e50a5c256c2075c1b44d219b7db0
Mark Kimsal <mark2@trash2009.com>
2009-10-14 12:38:27 -0400

    New installation procedure.

---
+++ README.txt
@@ -0,0 +1,35 @@
+There are 2 installation methods.
+
+When Should I Use the Simple Installation?
+================================================================================
+Use the simple installation if:
+  * You are not a developer or administrator
+  * This is your first time installing the plugin
+  * You have not made any changes to this module's translation files.
+  * You have not made any changes to this module's template files.
+
+
+When Should I Use the Advanced Installation?
+================================================================================
+Use the advanced installation if:
+  * You are a developer or administrator and you want to understand this module
+  * You have made changes to this module's translations
+  * You have made changes to this module's templates.
+
+
+How do I Install the Simple Installation?
+================================================================================
+Simply copy the "app/" folder to your Magento installation.  This will probalby 
+involve using an FTP client to copy the files up to your server.  Nothing 
+will be overwritten except any previous installation of the Metrof_FBConnect
+module.
+
+
+How do I Install the Advanced Installation?
+================================================================================
+Follow the instructions in the file "INSTALL"
+
+Both installation procedures contain the exact same files, but each installation 
+folder has the files in a different folder configuration.
+
+
--- mkzip.sh
+++ mkzip.sh
@@ -1,3 +1,49 @@
 #!/bin/bash
-cd ../../
-zip -r Metrof_FBConnect_magento-1.2.2.zip Metrof/FBConnect/ -x "Metrof/FBConnect/.git*" -x "*~" -x "Metrof/FBConnect/mkzip.sh" -x "Metrof/FBConnect/test_res.php"
+mkdir build/
+
+cd build
+
+ln -sf ../LICENSE .
+ln -sf ../INSTALL .
+ln -sf ../README.txt README.txt
+
+#simple install
+mkdir -p 1.simple.install/
+mkdir -p 1.simple.install/app/
+mkdir -p 1.simple.install/app/code/local/Metrof/FBConnect/
+mkdir -p 1.simple.install/app/design/frontend/default/default/layout/
+mkdir -p 1.simple.install/app/design/frontend/default/default/template/
+mkdir -p 1.simple.install/app/etc/modules
+ln -sf ../../../../../Metrof_FBConnect.xml 1.simple.install/app/etc/modules/
+ln -sf ../../../../../../../../design/frontend.layout/metrof_fbc.xml 1.simple.install/app/design/frontend/default/default/layout/
+ln -sf ../../../../../../../../design/frontend.templates/fbconnect 1.simple.install/app/design/frontend/default/default/template/
+#locale
+ln -sf ../../../locale/ 1.simple.install/app/
+
+#main module
+ln -sf ../../../../../../../controllers 1.simple.install/app/code/local/Metrof/FBConnect/
+ln -sf ../../../../../../../etc 1.simple.install/app/code/local/Metrof/FBConnect/
+ln -sf ../../../../../../../design 1.simple.install/app/code/local/Metrof/FBConnect/
+ln -sf ../../../../../../../Helper 1.simple.install/app/code/local/Metrof/FBConnect/
+ln -sf ../../../../../../../locale 1.simple.install/app/code/local/Metrof/FBConnect/
+ln -sf ../../../../../../../Metrof_FBConnect.xml 1.simple.install/app/code/local/Metrof/FBConnect/
+ln -sf ../../../../../../../sql 1.simple.install/app/code/local/Metrof/FBConnect/
+
+
+
+
+mkdir -p 2.advanced.install/Metrof/FBConnect/
+ln -sf ../../../../controllers 2.advanced.install/Metrof/FBConnect/
+ln -sf ../../../../etc 2.advanced.install/Metrof/FBConnect/
+ln -sf ../../../../design 2.advanced.install/Metrof/FBConnect/
+ln -sf ../../../../Helper 2.advanced.install/Metrof/FBConnect/
+ln -sf ../../../../locale 2.advanced.install/Metrof/FBConnect/
+ln -sf ../../../../sql 2.advanced.install/Metrof/FBConnect/
+ln -sf ../../../../Metrof_FBConnect.xml 2.advanced.install/Metrof/FBConnect/
+ln -sf ../../../../INSTALL 2.advanced.install/Metrof/FBConnect/
+ln -sf ../../../../LICENSE 2.advanced.install/Metrof/FBConnect/
+
+
+zip -r ../../../Metrof_FBConnect_magento-1.2.3.zip . -x "*~"
+#cd ../../
+#zip -r Metrof_FBConnect_magento-1.2.2.zip Metrof/FBConnect/ -x "Metrof/FBConnect/.git*" -x "*~" -x "Metrof/FBConnect/mkzip.sh" -x "Metrof/FBConnect/test_res.php"
