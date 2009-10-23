c0096c4ae8f4c0cc025cdbb8a2e39369f6631280
Mark Kimsal <mark2@trash2009.com>
2009-10-23 14:45:40 -0400

    Version bump to 1.3.0

--- mkzip.sh
+++ mkzip.sh
@@ -31,8 +31,6 @@ ln -sf ../../../../../../../Metrof_FBConnect.xml 1.simple.install/app/code/local
 ln -sf ../../../../../../../sql 1.simple.install/app/code/local/Metrof/FBConnect/
 
 
-
-
 mkdir -p 2.advanced.install/Metrof/FBConnect/
 ln -sf ../../../../controllers 2.advanced.install/Metrof/FBConnect/
 ln -sf ../../../../etc 2.advanced.install/Metrof/FBConnect/
@@ -46,6 +44,6 @@ ln -sf ../../../../INSTALL 2.advanced.install/Metrof/FBConnect/
 ln -sf ../../../../LICENSE 2.advanced.install/Metrof/FBConnect/
 
 
-zip -r ../../../Metrof_FBConnect_magento-1.2.9.zip . -x "*~"
+zip -r ../../../Metrof_FBConnect_magento-1.3.0.zip . -x "*~"
 #cd ../../
 #zip -r Metrof_FBConnect_magento-1.2.2.zip Metrof/FBConnect/ -x "Metrof/FBConnect/.git*" -x "*~" -x "Metrof/FBConnect/mkzip.sh" -x "Metrof/FBConnect/test_res.php"
